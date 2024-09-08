48cc84d7bf9352cdb0a00c8c07270c1fb5c28042
Kapobajza <kapobajza@gmail.com>
2024-09-08 09:19:15 +0200

    feat: Preparing deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo docker compose -f compose.yaml -f compose.prod.yaml -f ./server/compose.logging.yaml -f server/compose.secondary.yaml pull $1
+sudo docker compose -f compose.yaml -f compose.prod.yaml -f ./server/compose.logging.yaml -f server/compose.secondary.yaml up $1 -d --force-recreate
