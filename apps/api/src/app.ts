03cc6c7b2337e9d96ba20f6c5996647f4e0bf994
Kapobajza <kapobajza@gmail.com>
2024-09-09 12:51:41 +0200

    fix: session key path in API

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -13,6 +13,7 @@ import { type FastifyInstance } from "fastify";
 import type { Pool } from "pg";
 import { ZodError } from "zod";
 import FastifyAuth from "@fastify/auth";
+import { getRelativeMonoRepoPath } from "@zdnevnik/scripting";
 
 import type { AppEnv } from "./types";
 
@@ -71,7 +72,10 @@ export async function buildApp(
 
   await fastify.register(SecureSession, {
     key: fs.readFileSync(
-      path.resolve("..", opts.env.SESSION_KEY_FILE ?? "session_key"),
+      path.resolve(
+        getRelativeMonoRepoPath("api"),
+        opts.env.SESSION_KEY_FILE ?? "session_key",
+      ),
     ),
     cookieName: opts.env.SESSION_COOKIE_NAME,
     cookie: {
