1c6e6f186513b7ac43a26db2e0c00c3e60d51552
Kapobajza <kapobajza@gmail.com>
2024-09-09 12:14:51 +0200

    fix: Improved deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker compose -f compose.yaml -f compose.prod.yaml -f ./server/compose.logging.yaml -f server/compose.secondary.yaml pull $1
-sudo docker compose -f compose.yaml -f compose.prod.yaml -f ./server/compose.logging.yaml -f server/compose.secondary.yaml up $1 -d --force-recreate
+sudo docker compose -f compose.yaml -f compose.prod.yaml -f ./server/compose.logging.yaml -f server/compose.secondary.yaml up $1 -d --force-recreate --build
