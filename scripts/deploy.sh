7fdabe8738d8d54fa998e91d4d3db071afa02a27
Kapobajza <kapobajza@gmail.com>
2024-09-09 18:25:13 +0200

    chore: Updated rolling out strategy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo docker compose -f compose.yaml -f compose.prod.yaml -f ./server/compose.logging.yaml -f server/compose.secondary.yaml pull $1
-sudo docker stack deploy -c compose.yaml -c compose.prod.yaml -c ./server/compose.logging.yaml -c server/compose.secondary.yaml $1
+sudo docker compose -f compose.yaml -f compose.prod.yaml -f ./server/compose.logging.yaml -f server/compose.secondary.yaml up -d --scale $1=3
+sudo docker compose -f compose.yaml -f compose.prod.yaml -f ./server/compose.logging.yaml -f server/compose.secondary.yaml up -d --remove-orphans
