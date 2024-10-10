f5b5e4ddb839ac21339e89d6c4ae712458d1266d
Kapobajza <kapobajza@gmail.com>
2024-10-10 12:35:17 +0200

    fix: Improved deploy script and removed replicas from web compose file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+SCALE_ARGUMENT=""
+
+if [ $1 == "api" ]; then
+  SCALE_ARGUMENT="--scale $1=3"
+fi
+
 sudo docker compose -f compose.yaml -f compose.main.yaml -f compose.logging.yaml -f compose.main.logging.yaml pull $1
-sudo docker compose -f compose.yaml -f compose.main.yaml -f compose.logging.yaml -f compose.main.logging.yaml up $1 -d --scale $1=3
+sudo docker compose -f compose.yaml -f compose.main.yaml -f compose.logging.yaml -f compose.main.logging.yaml up $1 -d $SCALE_ARGUMENT
