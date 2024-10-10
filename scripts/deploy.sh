a2aebc999a6fbcdead7203d5a54b087cedca1776
Kapobajza <kapobajza@gmail.com>
2024-10-10 12:25:35 +0200

    revert: Brought back web replicas to main compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker compose -f compose.yaml -f compose.prod.yaml -f ./server/compose.logging.yaml -f server/compose.secondary.yaml pull $1
-sudo docker compose -f compose.yaml -f compose.prod.yaml -f ./server/compose.logging.yaml -f server/compose.secondary.yaml up $1 -d --scale $1=3
+sudo docker compose -f compose.yaml -f compose.main.yaml -f compose.logging.yaml -f compose.main.logging.yaml pull $1
+sudo docker compose -f compose.yaml -f compose.main.yaml -f compose.logging.yaml -f compose.main.logging.yaml up $1 -d --scale $1=3
